e8983a6dc939c709ccab45b69102ec1886387103
FalconPilot <fpilot.github@gmail.com>
2016-11-09 22:26:57 +0100

    append data handling

--- src/epitest/web/controllers/page_controller.ex
+++ src/epitest/web/controllers/page_controller.ex
@@ -1,28 +1,59 @@
 defmodule Epitest.PageController do
   use Epitest.Web, :controller
 
+  # Page display
+
   def index(conn, _params) do
-    conn
-    |> render "index.html"
+    render conn, "index.html"
   end
 
-  def fetch(conn, _params) do
-    url = "https://bugs-data.thomasdufour.fr:2847/0.1/modules?login=#{login}&pass=#{pass}"
-    case HTTPoison.get(url) do
-      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-        IO.puts "OK"
-      {:ok, %HTTPoison.Response{status_code: 404}} ->
-        IO.puts "Error 404 !"
-      {:error, %HTTPoison.Error{reason: reason}} ->
-        IO.inspect reason
-      stuff ->
-        IO.inspect stuff
+  # Fetching request
+
+  def fetch(conn, params) do
+    # For debug purposes, load data if it exists
+    data = get_json("priv/data/logs.json")
+    url = "https://bugs-data.thomasdufour.fr:2847/0.1/modules"
+    case request(url, params["fetch"]["login"], params["fetch"]["password"]) do
+      {:ok, data} ->
+        conn
+        |> assign(:data, data)
+        |> render("dataview.html")
+      {:error, message} ->
+        IO.inspect message
+        redirect conn, to: "/"
+      _ ->
+        redirect conn, to: "/"
     end
-    redirect conn, to: "/"
+  end
+
+  # HTTP Request
+
+  def request(url, login, password) do
+    case HTTPoison.get(url, [], [hackney: [basic_auth: {login, password}]]) do
+      {:ok, %HTTPoison.Response{status_code: 200, headers: headers, body: body}} ->
+        {:ok, unzip(headers, body)}
+      {:ok, %HTTPoison.Response{status_code: 401, headers: headers}} ->
+        {:error, "Error 401 : Unauthorized !"}
+      {:ok, %HTTPoison.Response{status_code: 404}} ->
+        {:error, "Error 404 : not found !"}
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        {:error, reason}
+      other ->
+        {:error, other}
+    end
+  end
+
+  # Data fetching
+
+  def unzip(headers, body) do
+    :zlib.gunzip(body)
+    |> Poison.decode!()
+    |> List.first
   end
 
   def get_json(filename) do
-    File.read!(filename) |> Poison.decode!()
+    File.read!(filename)
+    |> Poison.decode!()
   end
 
 end
