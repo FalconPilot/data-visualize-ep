d2461e19f38ef88d901d55429b4933f70dec57c3
FalconPilot <fpilot.github@gmail.com>
2016-11-10 11:22:48 +0100

    added working version

--- src/epitest-phoenix/web/controllers/page_controller.ex
+++ src/epitest-phoenix/web/controllers/page_controller.ex
@@ -13,14 +13,26 @@ defmodule Epitest.PageController do
     root = "https://bugs-data.thomasdufour.fr:2847/0.1/modules"
     login = params["fetch"]["login"]
     pass = params["fetch"]["password"]
-    params = %{burl: root, login: login, pass: pass}
+    date = params["fetch"]["date"]
     case request(root, login, pass) do
       {:ok, data} ->
-        modules = get_list data, "code"
-        projects = get_sub params, "name", modules
-        IO.inspect map_from_lists(modules, projects)
+        params =
+          %{burl: root,
+            login: login,
+            pass: pass,
+            year: date["year"],
+            date:
+              %{month: date["month"],
+                day: date["day"],
+                hour: date["hour"],
+                minute: date["minute"]}
+            }
+        data = get_list(data, "code")
+        |> get_sub(params)
+        |> get_test_results(params)
+        |> merge_maps
         conn
-        |> assign(:data, "Lel")
+        |> assign(:data, data)
         |> render("dataview.html")
       {:error, message} ->
         IO.puts message
@@ -28,18 +40,54 @@ defmodule Epitest.PageController do
     end
   end
 
-  # Create map from lists
+  # Merge maps list
 
-  def map_from_lists(list1, list2) do
-    map_from_lists list1, list2, []
+  def flatten_map(list) do
+    flatten_map list, []
   end
 
-  def map_from_lists([h1|t1], [h2|t2], acc) do
-    map_from_lists t1, t2, [%{module: h1, projects: h2}|acc]
+  def flatten_map([h|t], acc) do
+    if is_list(h) do
+      flatten_map t, flatten_map(h) ++ acc
+    else
+      flatten_map t, [h|acc]
+    end
   end
 
-  def map_from_lists([], [], acc) do
+  def flatten_map([], acc) do
     Enum.reverse acc
+  end
+
+  def merge_maps(list) do
+    flatmap = flatten_map(list)
+    merge_maps flatmap, %{}
+  end
+
+  def merge_maps([h|t], map) do
+    merge_maps t, update_map(map, h)
+  end
+
+  def merge_maps([], map) do
+    Map.merge(map, %{total: Enum.sum(Map.values(map))})
+  end
+
+  # Update map from patch
+
+  def update_map(map, patch) do
+    update_map(map, [:fail_nocrash, :fail_crash, :fail_unknown, :passed, :unknown], patch)
+  end
+
+  def update_map(map, [h|t], patch) do
+    case patch[h] do
+      nil ->
+        update_map map, t, patch
+      val ->
+        update_map Map.update(map, h, 1, &(&1+val)), t, patch
+    end
+  end
+
+  def update_map(map, [], _patch) do
+    map
   end
 
   # Get lists from keys
@@ -56,50 +104,134 @@ defmodule Epitest.PageController do
     Enum.reverse acc
   end
 
-  # Get sub-lists
+  # Get main list
 
-  def get_sub(params, key, modules) do
-    get_sub params, key, modules, []
+  def get_sub(modules, params) do
+    get_sub modules, params, []
   end
 
-  def get_sub(params, key, [h|t], acc) do
-    get_sub params, key, t, [get_all_projects(params, key, h)|acc]
+  def get_sub([h|t], params, acc) do
+    get_sub t, params, [%{module: h, projects: get_all_projects(h, params)}|acc]
   end
 
-  def get_sub(_params, _key, [], acc) do
+  def get_sub([], _params, acc) do
     Enum.reverse acc
   end
 
   # Get all projects
 
-  def get_all_projects(params, key, module) do
+  def get_all_projects(module, params) do
     url = "#{params.burl}/#{module}/projects"
     case request(url, params.login, params.pass) do
       {:ok, data} ->
         get_list data, "name"
       {:error, message} ->
         IO.puts message
-        []
+        {:error, message}
     end
   end
 
   # Get a single project test results
 
-  def get_test_results(params, module, suffix) do
-    url = "#{params.burl}/#{module}/projects/#{suffix}/testRuns/2015?before=2016-08-01T00:00:00Z"
+  def get_test_results(list, params) do
+    get_test_results list, params, []
+  end
+
+  def get_test_results([%{module: module, projects: list}|t], params, acc) do
+    get_test_results t, params, [get_result(list, module, params)|acc]
+  end
+
+  def get_test_results([], _params, acc) do
+    Enum.reverse acc
+  end
+
+  # Get results of a project list
+
+  def get_result(list, module, params) do
+    get_result list, module, params, []
+  end
+
+  def get_result([h|t], module, params, acc) do
+    get_result t, module, params, [get_test(module, h, params)|acc]
+  end
+
+  def get_result([], _module, _params, acc) do
+    Enum.reverse acc
+  end
+
+  def get_test(module, project, params) do
+    intYear = Kernel.elem(Integer.parse(params.year), 0)
+    date = format_date(params.date)
+    url = "#{params.burl}/#{module}/projects/#{project}/testRuns/#{params.year}?before=#{intYear+1}-#{date.month}-#{date.day}T#{date.hour}:#{date.minute}:00Z"
     case request(url, params.login, params.pass) do
       {:ok, data} ->
-        data
+        get_grades data
       {:error, message} ->
         IO.puts message
-        %{}
+        []
     end
   end
 
-  # Update results
+  # Format date params
 
-  def update_results(map, key) do
-    Map.update map, key, 1, &(&1 + 1)
+  def format_date(map) do
+    format_date Map.keys(map), map
+  end
+
+  def format_date([h|t], map) do
+    if byte_size(map[h]) == 1 do
+      newint = "0" <> map[h]
+    else
+      newint = map[h]
+    end
+    format_date t, Map.put(map, h, newint)
+  end
+
+  def format_date([], map) do
+    map
+  end
+
+  # Get test grades
+
+  def get_grades(list) do
+    get_grades list, %{}
+  end
+
+  def get_grades([%{"tests" => tests}|t], acc) do
+    get_grades t, Map.merge(parse_results(tests), acc)
+  end
+
+  def get_grades([%{"skills" => skills}|t], acc) do
+    get_grades t, Map.merge(get_grades(skills), acc)
+  end
+
+  def get_grades([], acc) do
+    acc
+  end
+
+  # Parse grade results
+
+  def parse_results(list) do
+    parse_results list, %{}
+  end
+
+  def parse_results([h|t], acc) do
+    case h do
+      %{"passed" => false, "crashed" => false} ->
+        parse_results t, Map.update(acc, :fail_nocrash, 1, &(&1+1))
+      %{"passed" => false, "crashed" => true} ->
+        parse_results t, Map.update(acc, :fail_crash, 1, &(&1+1))
+      %{"passed" => false} ->
+        parse_results t, Map.update(acc, :fail_unknown, 1, &(&1+1))
+      %{"passed" => true} ->
+        parse_results t, Map.update(acc, :passed, 1, &(&1+1))
+      _ ->
+        parse_results t, Map.update(acc, :unknown, 1, &(&1+1))
+    end
+  end
+
+  def parse_results([], acc) do
+    acc
   end
 
   # HTTP Request
--- src/epitest-phoenix/web/static/js/app.js
+++ src/epitest-phoenix/web/static/js/app.js
@@ -7,6 +7,35 @@
 // in vendor, which are never wrapped in imports and
 // therefore are always executed.
 
+document.addEventListener("DOMContentLoaded", function() {
+  initialize()
+})
+
+function initialize() {
+  let total = 0
+  let elems = ["passed", "fail_nocrash", "fail_crash"]
+  for (let i = 0; i < elems.length; i++) {
+    let elem = document.getElementById("data_" + elems[i])
+    total += parseInt(elem.textContent)
+  }
+
+  let percent = total/100
+  let max_height = 90
+
+  setTimeout(function() {
+    for (let i = 0; i < elems.length; i++) {
+      let elem = document.getElementById("bar_" + elems[i])
+      let data = document.getElementById("data_" + elems[i])
+      let value = parseInt(data.textContent)
+      let height = (value / total) * 100
+      elem.style.height = height + "%"
+      let footnote = document.createElement("div")
+      footnote.innerHTML = (Math.round(height*10)/10) + "%"
+      data.parentNode.appendChild(footnote)
+    }
+  }, 200)
+}
+
 // Import dependencies
 //
 // If you no longer want to use a dependency, remember
